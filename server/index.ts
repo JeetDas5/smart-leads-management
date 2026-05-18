98de7dfcd89639173ce6c34cd2134a5e276a506b
JeetDas5 <jeet15083011@gmail.com>
2026-05-18 13:29:54 +0530

    feat: server setup

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,24 @@
-console.log("Hello via Bun!");
+import dotenv from "dotenv";
+dotenv.config();
+
+import mongoose from "mongoose";
+import app from "./app.js";
+
+const PORT = process.env.PORT || 5000;
+
+const startServer = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI as string);
+
+    console.log("MongoDB Connected");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+startServer();
